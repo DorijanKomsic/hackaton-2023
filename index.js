859828ce5995b61e19ae5dcdd1d6bfb3a70ee436
DorijanKomsic <komsic.dorijan@gmail.com>
2023-03-18 22:45:08 +0100

    Register now works

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const bodyParser = require('body-parser');
+
 require('./dbConnection');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
