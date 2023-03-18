bdd35b350f37951344f2917da68aca4cfe3b318f
DorijanKomsic <komsic.dorijan@gmail.com>
2023-03-18 21:29:26 +0100

    Docker image needs needs work

--- dbConnection.js
+++ dbConnection.js
@@ -3,18 +3,17 @@ require('dotenv').config();
 
 mongoose.set('strictQuery', false);
 
-dbURI = 'mongodb://localhost:27017';
+dbURI = 'mongodb://127.0.0.1:27017/hackatonDB';
 //dbURI = 'mongodb://temp:user@localhost:27017/?authSource=hackatonDB'
 //dbURI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo27017/hackatonDB`;
 
 mongoose.connect(dbURI, {
+    useNewUrlParser: true,
     maxPoolSize: 50,
     useUnifiedTopology: true,
-    authSource: 'hackatonDB',
+    family: 4,
     //user: 'temp',
     //pass: 'user',
-    //user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    //pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
 });
 
 //mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017`);
