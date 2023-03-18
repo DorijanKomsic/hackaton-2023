5acefe1dc8584313568163fd2df5c015a62269b0
benjom22 <benjom@bih.net.ba>
2023-03-18 13:44:53 +0100

    User models made

---
+++ Users.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+const generalConfig = require('');
+
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: [true, 'Name can\'t be empty'],
+        minlength: 3,
+        maxlength: 25,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        required: [true, 'Lastname can\'t be empty'],
+        minlength: 4,
+        maxlength: 30,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Valid email required'
+        },
+        unique: true // creates an index in the db
+    },
+    password: {
+        type: String,
+        required: [true, 'Password can\'t be empty'],
+        minlength: 6,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        unique: true
+    }
+});
+
+UserSchema.pre('save', async function () {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    this.createdAt = new Date().toISOString();
+});
+
+
+UserSchema.methods.comparePassword = async function (pass) {
+    return await bcryptjs.compare(pass, this.password);
+}
+
+module.exports = mongoose.model("Users", UserSchema);
