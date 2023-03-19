a69a51590ffad7e7f27a0796b36451dfa407b7eb
DorijanKomsic <komsic.dorijan@gmail.com>
2023-03-19 09:35:19 +0100

    Added get request for listing profiles

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,11 @@
 const { profileRegister } = require("../services/ProfileService");
 const { profileLogin } = require("../services/ProfileService");
+const { listProfiles } = require("../services/ProfileService");
 
 const router = require("express").Router();
 
 router.post("/profileRegister", profileRegister);
 router.post("/profileLogin", profileLogin);
+router.get("/list-profiles", listProfiles);
 
 module.exports = router;
